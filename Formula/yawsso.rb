27bd5f4b85962f9343e816ae74fdae04468e12d8
Ivan Dyachkov <ivan.dyachkov@kry.se>
2022-05-13 11:56:00 +0200

    add version and other metadata

--- Formula/yawsso.rb
+++ Formula/yawsso.rb
@@ -1,11 +1,12 @@
 class Yawsso < Formula
   include Language::Python::Virtualenv
 
-  desc ""
-  homepage ""
+  desc "Yet Another AWS SSO - sync up AWS CLI v2 SSO login session to legacy CLI v1 credentials"
+  homepage "https://github.com/victorskl/yawsso"
   url "https://github.com/victorskl/yawsso/archive/refs/tags/0.7.2.tar.gz"
+  version "0.7.2"
   sha256 "a2f665d12f1d29095a3a103d61638308ea4e2184bce5810ad80eeebddffcfb51"
-  license ""
+  license "MIT"
 
   depends_on "python"
 
